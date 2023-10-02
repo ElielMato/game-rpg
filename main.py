cacf5fd0d2f87eafa38cea80d385240f507cca4c
[Facundo Merino] <[valid-email]>
2023-10-02 18:05:29 -0300

    cambios

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ class Character:
         self.diamonds = 150
         self.weapon = None
         self.mana = 100
-
+    
+    def __repr__(self):
+        description = ""
+        if self.name == "Guerrero":
+            description = "El guerrero es un luchador intrépido que se enfrenta a sus enemigos con valentía y fuerza bruta. Con su imponente fuerza, defiende la justicia y la gloria en el campo de batalla."
+        elif self.name == "Mago":
+            description = "El hechicero es un conjurador de secretos antiguos y magia oscura. Con su destreza en las artes ocultas, teje hechizos misteriosos que desafían las leyes de la realidad."
+        elif self.name == "Caballero":
+            description = "El caballero es un noble guerrero de corazón noble y valor inquebrantable. Con su armadura reluciente y su fiel espada, defiende el honor y la justicia en tiempos de peligro. Su lealtad y coraje son tan imponentes como su armadura brillante mientras cabalga en busca de aventuras y protege a los indefensos."
+        else:
+            description =  "El arquero es un experto tirador con una puntería excepcional. En la distancia, con su arco y flechas, es el guardián de la precisión y la velocidad, derribando a sus enemigos con elegancia mortal."
+        return description
 
 class Enemies:
     def __init__(self, name = "", health = int(0), damage = int(0), accuracy = int(0), difficulty = ""):
@@ -44,8 +55,6 @@ class Dungeons:
         self.name = name
         self.level = level
 
-name = ""
-main_character = None
 guerrero = Character(name, "Guerrero", 100, 15, 50)
 mago = Character(name, "Mago", 110, 10, 70)
 arquero = Character(name, "Arquero", 90, 15, 75)
@@ -148,3 +157,76 @@ enemy_abilities = [
 
 def Upgrade():
     pass
+
+#Enemigos
+enemigo_facil_1 = Enemies("Escurridizo", 100, "Fácil")
+enemigo_facil_2 = Enemies("Acechador", 110, "Fácil")
+enemigo_facil_3 = Enemies("Zumbador", 95, "Fácil")
+enemigo_facil_4 = Enemies("Sombrío", 120, "Fácil")
+enemigo_facil_5 = Enemies("Saltarín", 105, "Fácil")
+
+enemigo_intermedio_1 = Enemies("Guardián", 180, "Intermedio")
+enemigo_intermedio_2 = Enemies("Traidor", 170, "Intermedio")
+enemigo_intermedio_3 = Enemies("Intrépido", 190, "Intermedio")
+enemigo_intermedio_4 = Enemies("Serpenteante", 160, "Intermedio")
+enemigo_intermedio_5 = Enemies("Afilado", 175, "Intermedio")
+
+enemigo_dificil_1 = Enemies("Demonio", 250, "Difícil")
+enemigo_dificil_2 = Enemies("Voraz", 240, "Difícil")
+enemigo_dificil_3 = Enemies("Frenético", 230, "Difícil")
+enemigo_dificil_4 = Enemies("Acero Negro", 260, "Difícil")
+enemigo_dificil_5 = Enemies("Sombra Profunda", 220, "Difícil")
+
+enemigo_jefe_1 = Enemies("Emperador Oscuro", 950, "Jefe")
+enemigo_jefe_2 = Enemies("Pesadilla Ancestral", 1000, "Jefe")
+enemigo_jefe_3 = Enemies("Destructor de Mundos", 900, "Jefe")
+enemigo_jefe_4 = Enemies("Titán Enfurecido", 920, "Jefe")
+enemigo_jefe_5 = Enemies("Eterno Desafío", 980, "Jefe")
+
+#Dugeons
+dungeon1 = Dungeons("Dungeon 1", 1)
+dungeon2 = Dungeons("Dungeon 2", 2)
+dungeon3 = Dungeons("Dungeon 3", 3)
+dungeon4 = Dungeons("Dungeon 4", 4)
+dungeon5 = Dungeons("Dungeon 5", 5)
+
+#Pociones
+Object("Pociones de Vida I", "potion_heal", 400, 50)
+Object("Pociones de Vida II", "potion_heal", 800, 75)
+Object("Pociones de Mana I", "potion_mana", 200, 50)
+Object("Pociones de Mana II", "potion_mana", 400, 75)
+Object("Pociones de Armor I", "potion_armor", 600, 50)
+Object("Pociones de Mana II", "potion_armor", 1200, 100)
+Object("Estofado", "food_heal", 300, 40)
+Object("Filete", "food_heal", 200, 30)
+Object("Pan", "food_heal", 150, 25)
+
+player_name = ""
+player_character = None
+
+def introduction():
+    print("BIENVENIDOS A game_name!")
+    print("game_name es un juego basado ")
+    
+def character_introduction():
+    print("Informacion de los Personajes a elegir:")
+    
+def character_creator():
+    type = input("Elige que tipo de personajes utilizaras:\n1) Guerrero\n2) Mago\n3) Arquero\n4) Caballero")
+    if type == "1":
+        guerrero.name = name
+        player_character = guerrero
+    elif type == "2":
+        mago.name = name
+        player_character = mago
+    elif type == "3":
+        arquero.name = name
+        player_character = arquero
+    elif type == "4":
+        caballero.name = name
+        player_character = caballero
+    else:
+        print("Esa opcion no esta disponible, escriba un numero del 1 al 4.")
+        character_creator()
+        
+        
