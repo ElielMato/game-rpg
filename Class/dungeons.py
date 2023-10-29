940dd51ec0c49f8c9e4d4946e56e5c21152f3d5f
Facumerino03 <129800602+Facumerino03@users.noreply.github.com>
2023-10-29 13:24:11 -0300

    Delete Class directory

--- Class/dungeons.py
+++
@@ -1,4 +0,0 @@
-class Dungeons:
-    def __init__(self, name = "", level = int(0)):
-        self.name = name
-        self.level = level
--- Class/enemies.py
+++
@@ -1,7 +0,0 @@
-class Enemies:
-    def __init__(self, name = "", health = int(0), difficulty = ""):
-        self.name = name
-        self.health = health
-        self.abilities = ["Golpeo", "Habilidad 2", "Habilidad 3"]
-        self.mana = 100
-        self.difficulty = difficulty
